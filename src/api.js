c15439bf7da4cd432a45ce1b06c4d83d611577e5
Jvnc0503 <jvnc0503@gmail.com>
2024-06-22 16:25:57 -0500

    fetchs done

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL="/api";
+const URL="https://cepnq6rjbk.execute-api.us-east-1.amazonaws.com";
 
 function saveToken(token){
     console.log(token);
@@ -20,6 +20,14 @@ export function getRole(){
     return getDecodedToken().role;
 }
 
+export function isLoggedIn(){
+    return getToken() !== null;
+}
+
+export function isAdmin(){
+    return getRole() === 'ROLE_ADMIN';
+}
+
 export const fetchLogin = async (body) =>{
     const response = await axios.post(`${URL}/auth/login`, body);
     saveToken(response.data.token);
@@ -34,4 +42,81 @@ export const fetchRegister = async (body) =>{
 
 export const fetchLogout = () =>{
     localStorage.removeItem('token');
+}
+
+export const fetchCreateItem = async (body) =>{
+    const response = await axios.post(`${URL}/items`, body, {
+        headers:{
+            Authorization: `Bearer ${getToken()}`
+        }
+    });
+    console.log(response.data);
+    return response.data.itemId;
+}
+
+export const fetchPutItem = async (body) =>{
+    const response = await axios.put(`${URL}/items`, body, {
+        headers:{
+            Authorization: `Bearer ${getToken()}`
+        }
+    });
+    return response.data;
+}
+
+export const fetchDeleteItem = async (id) =>{
+    const response = await axios.delete(`${URL}/items/${id}`, {
+        headers:{
+            Authorization: `Bearer ${getToken()}`
+        }
+    });
+    return response.data;
+}
+
+export const fetchGetItem = async (id) =>{
+    const response = await axios.get(`${URL}/items/${id}`, {
+        headers:{
+            Authorization: `Bearer ${getToken()}`
+        }
+    });
+    return response.data;
+}
+
+export const fetchGetItems = async (limit, lastKey) =>{
+    const response = await axios.get(`${URL}/items?limit=${limit}&lastKey=${lastKey}`);
+    console.log(response.data);
+    return response.data.lastKey;
+}
+
+export const fetchBuy = async (body) =>{
+    const response = await axios.post(`${URL}/buy`, body, {
+        headers:{
+            Authorization: `Bearer ${getToken()}`
+        }
+    });
+    return response.data;
+}
+
+export const fetchAddCart = async (body) =>{
+    const response = await axios.post(`${URL}/cart`, body, {
+        headers:{
+            Authorization: `Bearer ${getToken()}`
+        }
+    });
+    return response.data;
+}
+
+export const fetchDeleteCartItem = async (body) =>{
+    const response = await axios.delete(`${URL}/cart`, body, {
+        headers:{
+            Authorization: `Bearer ${getToken()}`
+        }
+    });
+}
+
+export const fetchGetCart = async (id) =>{
+    const response = await axios.get(`${URL}/cart/${id}`, {
+        headers:{
+            Authorization: `Bearer ${getToken()}`
+        }
+    });
 }
